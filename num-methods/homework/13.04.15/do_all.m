fa2ef4176e35f0700458a3f5aece9e22f4867884
Vitaly Rybnikov <Vitaly.Rybnikov@gmail.com>
2013-05-07 20:27:03 +0400

    add homework - num-methods - 15-apr-2013

---
+++ num-methods/homework/13.04.15/do_all.m
@@ -0,0 +1,14 @@
+% Do and Print all homework's tasks
+%
+% Тема : Численные методы решения ОДУ
+%
+% Copyright (C) 2013 Frodox
+% Created: 2013-04-14
+
+disp(''); disp('== Task 1:'); disp('');
+task1_0;
+task1_1;
+task1_2;
+
+disp(''); disp('== Task 2:'); disp('');
+task2;
---
+++ num-methods/homework/13.04.15/f.m
@@ -0,0 +1,7 @@
+function z = f(x, y)
+
+% Calculate y' = f(x, y) = y - 2*x / y
+%
+% Тема : Численные методы решения ОДУ
+
+z = y - 2*x / y;
