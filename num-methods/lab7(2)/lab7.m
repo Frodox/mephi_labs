7ace0ae6f9f55907cc60e6c2028fda1227f34194
Vitaly Rybnikov <Vitaly.Rybnikov@gmail.com>
2013-05-08 20:16:17 +0400

    Realize of getting transformation matrix from E (== eye(1))

--- num-methods/lab7(2)/lab7.m
+++ num-methods/lab7(2)/lab7.m
@@ -12,9 +12,31 @@ clear('all');
 init_data
 
 % task 1 - get eigen values of matrix A by yacobi-method
-eig_val = get_eigenvalues_yacobi(A);
+%eig_val = get_eigenvalues_yacobi(A);
 
 % task 2 - get characteristic polynomial by Danilevsky method
 
+B = A; % don't edit original A
+N = size(B)(1);
+    printf("=============================================\n\n");
+    A
 
+E = eye(N);
+b_n_n_1 = B(N, N-1)
+if b_n_n_1 ~= 0
+
+    B(:, N-1) = B(:, N-1) / b_n_n_1;
+    E(:, N-1) = E(:, N-1) / b_n_n_1;
+
+    for j = 1:N
+	if j ~= N-1
+	    b_nj = B(N, j);
+	    B(:, j) = B(:, j) - b_nj * B(:, N-1);	% B(N, j) have changed
+	    E(:, j) = E(:, j) - b_nj * E(:, N-1);
+	end
+    end
+    
+else 
+    
+end
 
