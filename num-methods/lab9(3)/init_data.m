0e178a1a0a15c0d128f04fff0dae2e06b8521a21
Vitaly Rybnikov <Vitaly.Rybnikov@gmail.com>
2013-05-11 01:06:04 +0400

    Init lab9

---
+++ num-methods/lab9(3)/init_data.m
@@ -0,0 +1,46 @@
+ % Initialize data for lab9(3)
+ % variant: 6 / 2
+
+%  This program is free software; you can redistribute it and/or modify
+%  it under the terms of the GNU General Public License as published by
+%  the Free Software Foundation; either version 2 of the License, or
+%  (at your option) any later version.
+%  
+%  This program is distributed in the hope that it will be useful,
+%  but WITHOUT ANY WARRANTY; without even the implied warranty of
+%  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+%  GNU General Public License for more details.
+%  
+%  You should have received a copy of the GNU General Public License
+%  along with this program; if not, write to the Free Software
+%  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+%  MA 02110-1301, USA.
+%  
+%  init_data.m
+%
+% Created : 2013-05-10
+% Copyright : Frodox <Frodox@lavabit.com>
+
+% I = integrate(0, 1){ exp(x) / [ 1 + exp(2x) ] } dx
+% Need to calc `I` with help of Sympson-method
+
+% Input data:
+a = 0;
+b = 1;
+
+% eps= input('Введите погрешность. eps = ');
+eps= 2e-3;
+if eps > 1e-2
+    error("Слишком маленькая погрешность. Не хочу работать.");
+end
+
+max_ab_d4f = 2.5;	% calculated by hand : max(a,b) | d4f(x) / dx |
+
+h = eps * 180 / ( (b-a) * max_ab_d4f );
+n = (b - a) / h;	% it's MINimum count of parts
+n = ceil(n);
+if 1 == mod(n, 2)
+    n++;
+end
+h = (b-a) / n;
+
