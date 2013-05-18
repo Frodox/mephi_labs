c4e67dfddeba827ddd6fe05b9fb00bc99155b27a
Vitaly Rybnikov <Vitaly.Rybnikov@gmail.com>
2013-05-19 01:04:12 +0400

    Init lab11

---
+++ num-methods/lab11(5)/init_data.m
@@ -0,0 +1,32 @@
+ % Initialize data for lab11(5)
+ % variant: 5.6
+
+%  This program is free software; you can redistribute it and/or modify
+%  it under the terms of the GNU General Public License as published by
+%  the Free Software Foundation; either version 2 of the License, or
+%  (at your option) any later version.
+%  
+%  This program is distributed in the hope that it will be useful,
+%  but WITHOUT ANY WARRANTY; without even the implied warranty of
+%  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+%  GNU General Public License for more details.
+%  
+%  You should have received a copy of the GNU General Public License
+%  along with this program; if not, write to the Free Software
+%  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+%  MA 02110-1301, USA.
+%  
+%  init_data.m
+%
+% Created : 2013-05-19
+% Copyright : Frodox <Frodox@lavabit.com>
+
+
+% == Task ==
+% f(x) = y' = y*x^2/2
+%
+% Solve it (get table of values) with method of prediction and correction - II 
+
+
+% == Input data ==
+
---
+++ num-methods/lab11(5)/lab11.m
@@ -0,0 +1,32 @@
+% * Численные методы
+% * Лабораторная работа No.11(5)
+% * Методы решения Обыкновенных Дифференциальных Уравненийетоды
+% - Вариант 5.6
+
+%  This program is free software; you can redistribute it and/or modify
+%  it under the terms of the GNU General Public License as published by
+%  the Free Software Foundation; either version 2 of the License, or
+%  (at your option) any later version.
+%
+%  This program is distributed in the hope that it will be useful,
+%  but WITHOUT ANY WARRANTY; without even the implied warranty of
+%  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+%  GNU General Public License for more details.
+%
+%  You should have received a copy of the GNU General Public License
+%  along with this program; if not, write to the Free Software
+%  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+%  MA 02110-1301, USA.
+%
+%  lab11.m
+
+% Created : 2013-05-19
+% Copyright : Frodox <Frodox@lavabit.com>
+
+clear('all');
+init_data;
+
+
+
+% Output --------------------------------|
+
