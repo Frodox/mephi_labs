c8ab61c35f37504d30cdb229089b36460f3f2803
Cristian <Cristian@local>
2013-10-25 10:59:28 +0400

    Remove extra code

--- bash/lab3/lab3.sh
+++ bash/lab3/lab3.sh
@@ -8,35 +8,12 @@
 # начинающиеся на ‘a’, заменив первый символ на заглавный.
 
 
-#!bin/bash
-rename_a_files()
-{
-    dir=$1    # dir - is folder
-    cd $dir
-
-    ls -A | while read item; do
-
-            if [[ -d $item ]] ; then
-                (rename_a_files $item)
-            else
-                echo "File: $item"
-                echo $f | grep '^a'
-                if [[ $? == 0 ]]; then
-                    newf=`echo $f | sed 's/^a/b'`
-                    mv $f $newf
-                fi
-            fi
-        done
-}
-
-#rename_a_files .
-
 find . -type f | while read fpath; do
 
     new_path=$(echo "$fpath" |sed 's_^\(.*/\)\([aа]\)\(.*\)_\1\u\2\3_')
 
     if [[ $new_path != $fpath ]]; then
-        # echo "Do mv for $new_path from $fpath"
+        # echo "Do mv to $new_path from $fpath"
 
         mv "$fpath" "$new_path"
 
