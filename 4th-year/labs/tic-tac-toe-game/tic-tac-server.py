4c56a283509407edd9839906e2ea6b199ee41521
Christian <Christian@bitthinker.com>
2014-03-17 23:26:37 +0400

    upd template. use common file with config

---
+++ 4th-year/labs/tic-tac-toe-game/tic-tac-server.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from __future__ import print_function
+import sys
+import tic_tac_common as tt_common
+
+
+# ---------------------------------------------------------------------------- #
+
+def main():
+	tt_common.say_hello("server")
+
+# ---------------------------------------------------------------------------- #
+
+if __name__ == "__main__":
+	main()
---
+++ 4th-year/labs/tic-tac-toe-game/tic_tac_common.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+Doc
+"""
+
+from __future__ import print_function
+import sys
+
+def say_hello (who_am_i):
+	print("Hello, it's a {0}".format(who_am_i))
+
